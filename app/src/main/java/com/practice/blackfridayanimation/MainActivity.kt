a234bcfb4cd5ce92c016702155c4ff3376fcd7d5
paparym <paparym47@gmail.com>
2022-09-28 12:22:41 +0300

    setup

--- app/src/main/java/com/practice/blackfridayanimation/MainActivity.kt
+++ app/src/main/java/com/practice/blackfridayanimation/MainActivity.kt
@@ -4,22 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.snap
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import com.practice.blackfridayanimation.ui.theme.BlackfridayanimationTheme
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 
@@ -29,18 +31,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             var personList by remember {
                 mutableStateOf(
-                    listOf(
-                        Person(firstName = "Mark", age = 22)
-//                        Person(firstName = "Mark", age = 23),
-//                        Person(firstName = "Mark", age = 24),
-//                        Person(firstName = "Mark", age = 25),
-//                        Person(firstName = "Mark", age = 26),
-//                        Person(firstName = "Mark", age = 27),
-//                        Person(firstName = "Mark", age = 28),
-//                        Person(firstName = "Mark", age = 29),
-//                        Person(firstName = "Mark", age = 30)
-                    )
+                    emptyList<Person>()
                 )
+            }
+            LaunchedEffect(Unit) {
+                while (personList.count() < 11) {
+                    val newPerson = Person(firstName = "Mark", age = Random.nextInt())
+                    val newList = ArrayList(personList)
+                    newList.add(0, newPerson)
+                    personList = newList
+                    delay(1540)
+                }
             }
             BlackfridayanimationTheme {
                 Column {
@@ -60,7 +61,6 @@ class MainActivity : ComponentActivity() {
                         Text(text = "add")
                     }
                 }
-
             }
         }
     }
@@ -98,43 +98,56 @@ fun ComposeGrid() {
 fun JustGrid(
     personList: List<Person>
 ) {
-    Box {
-        LazyVerticalGrid(
-            modifier = Modifier,
-            columns = GridCells.Fixed(6)
-        ) {
-            items(items = personList, key = { it.age }) {
-                val alphaAnimation = remember { Animatable(0f) }
-                val xAnimation = remember { Animatable(-100f) }
-                LaunchedEffect(Unit) {
-                    launch {
-                        xAnimation.animateTo(
-                            targetValue = 0f,
-                            animationSpec = tween(
-                                durationMillis = 500
-                            )
+    val configuration = LocalConfiguration.current
+    val itemWidth = configuration.screenWidthDp / 6
+    val itemHeight = 84.dp
+    LazyRow(
+        userScrollEnabled = false,
+        modifier = Modifier.requiredWidth((10 * itemWidth).dp)
+    ) {
+        items(items = personList, key = { it.age }) {
+            val alphaAnimation = remember { Animatable(0f) }
+            val xAnimation = remember { Animatable(-itemWidth - 12f) }
+            LaunchedEffect(Unit) {
+                launch {
+                    xAnimation.animateTo(
+                        targetValue = 0f,
+                        animationSpec = tween(
+                            durationMillis = 1500,
+                            easing = LinearEasing
                         )
-                    }
-                    launch {
-                        alphaAnimation.animateTo(
-                            targetValue = 1f,
-                            animationSpec = tween(
-                                durationMillis = 1000
-                            )
+                    )
+                }
+                launch {
+                    alphaAnimation.animateTo(
+                        targetValue = 1f,
+                        animationSpec = tween(
+                            durationMillis = 500,
+                            easing = LinearEasing
                         )
-                    }
+                    )
                 }
-                Box(
-                    modifier = Modifier
-                        .offset(x = xAnimation.value.dp)
-                        .alpha(alphaAnimation.value)
-                        .padding(4.dp)
-                        .animateItemPlacement()
-                        .background(Color.Blue)
-                        .size(width = 54.dp, height = 82.dp)
-                ) {
-                    Text(text = "${it.firstName} ${it.age}")
-                }
+            }
+            val index = personList.indexOf(it)
+            Box(
+                modifier = Modifier
+                    .offset(x = xAnimation.value.dp)
+//                        .alpha(alphaAnimation.value)
+                    .padding(4.dp)
+                    .animateItemPlacement(
+                        animationSpec = if (index == 5) {
+                            snap()
+                        } else {
+                            tween(
+                                durationMillis = 1500,
+                                easing = LinearEasing
+                            )
+                        }
+                    )
+                    .background(Color.Blue)
+                    .size(width = itemWidth.dp, height = itemHeight)
+            ) {
+                Text(text = "${it.firstName} ${it.age}")
             }
         }
     }
