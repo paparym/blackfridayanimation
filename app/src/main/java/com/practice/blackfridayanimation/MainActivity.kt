16e4eb001ceaec3e17e0f842988467e462b1deec
paparym <paparym47@gmail.com>
2022-09-29 12:07:17 +0300

    something really close to what I need

--- app/src/main/java/com/practice/blackfridayanimation/MainActivity.kt
+++ app/src/main/java/com/practice/blackfridayanimation/MainActivity.kt
@@ -1,52 +1,137 @@
 package com.practice.blackfridayanimation
 
 import android.os.Bundle
+import android.util.Log
+import android.view.animation.LinearInterpolator
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var ticketsAdapter: TicketsAdapter
 
+    private var ticketList = mutableListOf<Ticket>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val recycler = findViewById<RecyclerView>(R.id.recyclerView)
+        recycler.itemAnimator = MyItemAnimator()
 
         ticketsAdapter = TicketsAdapter(lifecycleScope)
         recycler.adapter = ticketsAdapter
-        ticketsAdapter.submitList(
-            listOf(
-                TicketRow(
-                    spanIndex = 0,
-                    ticketList = listOf(
-                        Ticket(1),
-                        Ticket(2),
-                        Ticket(3),
-                        Ticket(4),
-                        Ticket(5),
-                        Ticket(6),
-                        Ticket(7),
-                        Ticket(8)
-                    )
-                ),
-                TicketRow(
-                    spanIndex = 1,
-                    ticketList = listOf(
-                        Ticket(7),
-                        Ticket(8)
-                    )
-                )
-            )
-        )
-//        lifecycleScope.launch {
-//            delay(100)
-//            recycler.x = -recycler.width.toFloat()
-//            recycler.animate()
-//                .setDuration(3000)
-//                .translationX(0f)
-//                .start()
-//        }
+        lifecycleScope.launch {
+            repeat(20) {
+                val newList = ArrayList(ticketList)
+                newList.add(0, Ticket(id = it))
+                ticketList = newList
+                ticketsAdapter.submitList(ticketList)
+                delay(1000L)
+            }
+        }
     }
+}
+
+class MyItemAnimator : DefaultItemAnimator() {
+    override fun animateAdd(holder: RecyclerView.ViewHolder): Boolean {
+        Log.d("-->", "x: ${holder.itemView.x}")
+        holder.itemView.x = -holder.itemView.width.toFloat() - 11f
+        holder.itemView.animate()
+            .setInterpolator(LinearInterpolator())
+            .setDuration(1000L)
+            .translationXBy(holder.itemView.width.toFloat() + 22f)
+            .start()
+        return true
+    }
+
+    override fun animateMove(
+        holder: RecyclerView.ViewHolder,
+        fromX: Int,
+        fromY: Int,
+        toX: Int,
+        toY: Int
+    ): Boolean {
+//        val listFiltered =
+//            list.filter { holder.bindingAdapterPosition % 5 == 0 && holder.bindingAdapterPosition != 0 }
+//        val mappedList = listFiltered
+//            .map { it + listFiltered.indexOf(it) }
+        return if (holder.bindingAdapterPosition % 6 == 0 && holder.bindingAdapterPosition != 0) {
+//            val view = holder.itemView
+//            val deltaX = toX - fromX
+//            val deltaY = toY - fromY
+//            if (deltaX != 0) {
+//                view.animate().translationX(0f)
+//            }
+//            if (deltaY != 0) {
+//                view.animate().translationY(0f)
+//            }
+//
+//            val animation = view.animate()
+//            mMoveAnimations.add(holder)
+//            animation.setDuration(moveDuration).setInterpolator(LinearInterpolator())
+//                .setListener(object : AnimatorListenerAdapter() {
+//                    override fun onAnimationStart(animator: Animator) {
+//                        dispatchMoveStarting(holder)
+//                    }
+//
+//                    override fun onAnimationCancel(animator: Animator) {
+//                        if (deltaX != 0) {
+//                            view.translationX = 0f
+//                        }
+//                        if (deltaY != 0) {
+//                            view.translationY = 0f
+//                        }
+//                    }
+//
+//                    override fun onAnimationEnd(animator: Animator) {
+//                        animation.setListener(null)
+//                        dispatchMoveFinished(holder)
+//                        mMoveAnimations.remove(holder)
+//                        dispatchFinishedWhenDone()
+//                    }
+//                }).start()
+
+            holder.itemView.x = fromX.toFloat()
+            holder.itemView.y = fromY.toFloat()
+//            holder.itemView.animate().translationX(0f)
+            holder.itemView.animate()
+                .setInterpolator(LinearInterpolator())
+                .alpha(0.0f)
+                .setDuration(1000L)
+                .translationYBy(0f)
+                .translationXBy(holder.itemView.width.toFloat() + 22f)
+                .withEndAction {
+                    holder.itemView.y = toY.toFloat()
+                    holder.itemView.alpha = 1f
+//                    holder.itemView.x = 0f - holder.itemView.width.toFloat()
+
+                    holder.itemView.x = -holder.itemView.width.toFloat() - 11f
+                    holder.itemView.animate()
+                        .setInterpolator(LinearInterpolator())
+                        .setDuration(1000L)
+                        .translationXBy(holder.itemView.width.toFloat() + 22f)
+                        .start()
+                }
+                .start()
+            false
+        } else {
+            holder.itemView.x = fromX.toFloat()
+            holder.itemView.y = fromY.toFloat()
+//            holder.itemView.animate().translationX(0f)
+            holder.itemView.animate()
+                .setInterpolator(LinearInterpolator())
+//                .alpha(0.0f)
+                .setDuration(1000L)
+                .translationYBy(0f)
+                .translationXBy((toX - fromX).toFloat())
+                .start()
+            return false
+        }
+    }
+
+    override fun getMoveDuration() = 1000L
 }
--- app/src/main/java/com/practice/blackfridayanimation/TicketsAdapter.kt
+++ app/src/main/java/com/practice/blackfridayanimation/TicketsAdapter.kt
@@ -3,26 +3,19 @@ package com.practice.blackfridayanimation
 import android.content.Context
 import android.os.Build
 import android.util.DisplayMetrics
-import android.view.Gravity.CENTER
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.WindowManager
-import android.widget.FrameLayout
-import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.core.view.updateLayoutParams
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.practice.blackfridayanimation.databinding.TicketRowItemBinding
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class TicketsAdapter(private val lifecycleScope: CoroutineScope) :
-    ListAdapter<TicketRow, TicketsAdapter.TicketsViewHolder>(DiffUtilCallback()) {
+    ListAdapter<Ticket, TicketsAdapter.TicketsViewHolder>(DiffUtilCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TicketsViewHolder {
         return TicketsViewHolder(
@@ -40,39 +33,9 @@ class TicketsAdapter(private val lifecycleScope: CoroutineScope) :
 
     inner class TicketsViewHolder(private val binding: TicketRowItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(ticketsRow: TicketRow) {
-            ticketsRow.ticketList.forEach {
-                // imageView
-                val imageView = FrameLayout(binding.root.context)
-                val viewWidth = getScreenWidth(binding.root.context) / 6
-                binding.root.updateLayoutParams {
-                    width = ticketsRow.ticketList.count() * viewWidth
-                }
-                imageView.setBackgroundColor(R.color.black)
-                val imageViewParams: LinearLayout.LayoutParams =
-                    LinearLayout.LayoutParams(viewWidth, MATCH_PARENT)
-                imageViewParams.gravity = CENTER
-                imageView.layoutParams = imageViewParams
-
-                // textView
-                val textView = TextView(imageView.context)
-                textView.layoutParams = ViewGroup.LayoutParams(MATCH_PARENT, MATCH_PARENT)
-                textView.setTextColor(R.color.white)
-                textView.text = it.id.toString()
-                textView.gravity = CENTER
-
-                binding.root.addView(imageView)
-                imageView.addView(textView)
-
-                lifecycleScope.launch(Dispatchers.Main) {
-                    delay(200)
-                    binding.root.x = -binding.root.width.toFloat()
-                    binding.root.animate()
-                        .translationX(0f)
-                        .setDuration(3000)
-                        .start()
-                }
-            }
+        fun bind(ticketsRow: Ticket) {
+//            if (ticketsRow.id == 0) binding.root.visibility = View.GONE else View.VISIBLE
+            binding.tvId.text = ticketsRow.id.toString()
         }
     }
 
@@ -90,13 +53,13 @@ class TicketsAdapter(private val lifecycleScope: CoroutineScope) :
         }
     }
 
-    class DiffUtilCallback : DiffUtil.ItemCallback<TicketRow>() {
-        override fun areItemsTheSame(oldItem: TicketRow, newItem: TicketRow): Boolean {
+    class DiffUtilCallback : DiffUtil.ItemCallback<Ticket>() {
+        override fun areItemsTheSame(oldItem: Ticket, newItem: Ticket): Boolean {
             return oldItem == newItem
         }
 
-        override fun areContentsTheSame(oldItem: TicketRow, newItem: TicketRow): Boolean {
-            return oldItem == newItem
+        override fun areContentsTheSame(oldItem: Ticket, newItem: Ticket): Boolean {
+            return oldItem.id == newItem.id
         }
     }
 }
